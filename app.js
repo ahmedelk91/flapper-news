75182cb9ac92c4ff477c723ed338fbfffbb1ae4a
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2018-05-28 12:54:01 -0400

    Added posts state

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-angular.module('flapperNews', ['ui-router'])
+angular.module('flapperNews', ['ui.router'])
 .config([
   '$stateProvider',
   '$urlRouterProvider',
@@ -8,39 +8,44 @@ angular.module('flapperNews', ['ui-router'])
       url:'/home',
       templateUrl:'/home.html',
       controller:'MainCtrl'
+    })
+    .state('posts', {
+      url:'/posts/{id}',
+      templateUrl:'/posts.html',
+      controller: 'PostsCtrl'
     });
 
-    $urlRouterProvider.otherwise('home')
+    $urlRouterProvider.otherwise('/home')
   }])
-.factory('posts', [function(){
-  var o = {
-    posts: []
-  };
-  return o;
-}])
-
-.controller('MainCtrl', [
-  '$scope',
-  'posts',
-  // Scope function to display posts
-  function($scope, posts){
-
-    $scope.posts = posts.posts;
-
-    // Appends new post to the scope.posts variable
-    $scope.addPost = function(){
-      // Prevents user from submitting blank title
-      if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
-        title: $scope.title,
-        link: $scope.link,
-        upvotes: 0
-      });
-      $scope.title = '';
-      $scope.link = '';
+  .factory('posts', [function(){
+    var o = {
+      posts: []
     };
+    return o;
+  }])
 
-    $scope.incrementUpvotes = function(post){
-      post.upvotes += 1;
-    };
-  }]);
+  .controller('MainCtrl', [
+    '$scope',
+    'posts',
+    // Scope function to display posts
+    function($scope, posts){
+
+      $scope.posts = posts.posts;
+
+      // Appends new post to the scope.posts variable
+      $scope.addPost = function(){
+        // Prevents user from submitting blank title
+        if(!$scope.title || $scope.title === '') { return; }
+        $scope.posts.push({
+          title: $scope.title,
+          link: $scope.link,
+          upvotes: 0
+        });
+        $scope.title = '';
+        $scope.link = '';
+      };
+
+      $scope.incrementUpvotes = function(post){
+        post.upvotes += 1;
+      };
+    }]);
